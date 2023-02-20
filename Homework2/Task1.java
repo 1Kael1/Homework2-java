a76f7eaeb9581cc1f55300bfc6c74d5a872e1a6f
Kael <churushkin.kirill@mail.ru>
2023-02-20 19:26:35 +0300

    first commit

---
+++ Homework2/Task1.java
@@ -0,0 +1,71 @@
+package Homework2;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Task1 {
+    public static void main(String[] args) {
+        // 1. Создать метод, который проверяет, является ли строка палиндромом.
+        // Палиндром - шалаш
+        System.out.println(isPalindrome("strts"));
+        System.out.println(isPalindrome("strtsa"));
+        System.out.println("");
+
+        // 2. С помощью Java создать файл temp.txt, и записать в него слово TEST 100
+        // раз.
+        // Если уже файл создан, то перезаписываем его.
+        Path path = Path.of("Homeworks", "Homework2", "temp.txt");
+        Path directoryPath = Path.of("Homeworks", "Homework2");
+        String text = "TEST";
+        int repeat = 100;
+        
+        String fileString = getString(text, repeat);
+
+        if (!Files.exists(path)) {
+            try {
+                Files.createDirectories(directoryPath);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            createFile(path);
+        }
+        fillFile(path, fileString);
+    }
+
+    public static boolean isPalindrome(String str) {
+        for (int i = 0; i < str.length() / 2; i++) {
+            if (str.charAt(i) != str.charAt(str.length() - 1 - i))
+                return false;
+        }
+        return true;
+    }
+
+    public static void createFile(Path path) {
+        try {
+            Files.createFile(path);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void fillFile(Path path, String text) {
+        try {
+            Files.writeString(path, text);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String getString(String text, int repeat) {
+        StringBuilder newString = new StringBuilder();
+
+        for (int i = 0; i < repeat / 10; i++) {
+            for (int j = 0; j < 9; j++) {
+                newString.append(text + " ");
+            }
+            newString.append(text + "\n");
+        }
+        return newString.toString();
+    }
+}
